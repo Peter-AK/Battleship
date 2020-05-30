64a5eb025b839f2cbbe19a016768b481af548f58
Peter-AK <pioter26@hotmail.com>
2020-05-29 22:01:16 -0400

    2020-may-29: Updated ship placement

--- players.py
+++ players.py
@@ -14,12 +14,23 @@ def valid_entry(entry):
         return True
 
 
-def numeric_entry(entry):
+def to_alpha_numeric(entry):
     """
     Transforms a grid position into a matrix position (IE: A3 --> [0, 3]
     """
+    grid_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G',
+            7: 'H', 8: 'I', 9: 'J'}
+    # Columns then rows
+    entry = [grid_dict[entry[1]] + str(entry[0] + 1)]
+    return entry
+
+
+def to_numeric(entry):
+    """
+        Transforms a grid position into a matrix position (IE: A3 --> [0, 3]
+        """
     grid_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6,
-            'H': 7, 'I': 8, 'J': 9}
+                 'H': 7, 'I': 8, 'J': 9}
     # Columns then rows
     entry = [int(entry[1]) - 1, grid_dict[entry[0]]]
     return entry
@@ -32,16 +43,18 @@ def select_dict(list):
     :param list:
     :return: dictionary
     """
-    my_dict = {}
+    my_dict_matrix = {}
+    my_dict_print = {}
     for i in range(len(list)):
-        my_dict[i + 1] = list[i]
+        my_dict_matrix[i + 1] = list[i]
+        my_dict_print[i + 1] = to_alpha_numeric(list[i])
+    return my_dict_matrix, my_dict_print
 
-    return my_dict
 
 class Players:
     def __init__(self, name):
         self.name = name
-        self.gird = np.zeros((10, 10))
+        self.grid = np.zeros((10, 10))
         self.non_placed_ships = [
                                  ['AirCraft Carrier', 5],
                                  ['BattleShip', 4],
@@ -62,7 +75,8 @@ class Players:
         the gird
          """
         for ship in self.non_placed_ships:
-            print(self.gird)
+            print(self.grid)
+
             self.place_ship(ship)
 
     def place_ship(self, ship):
@@ -70,24 +84,28 @@ class Players:
         Asks the user for an anchor point and an end point for each
         ship that has not been placed.
         """
-        valid_loc = False
 
-        while valid_loc is False:
+        while True:
             start_loc = input('Please enter the anchor point '
                               'for the {ship} with a size of {size}: [example '
                               '"A3"]: '
                               .format(ship=ship[0], size=ship[1]))
             start_loc = start_loc.upper()
-            if valid_entry(start_loc):
-                start_loc = numeric_entry(start_loc)
-                valid_end_loc = self.is_free(start_loc, ship)
-                valid_end_loc = select_dict(valid_end_loc)
-                print(valid_end_loc)
-                max = len(valid_end_loc)
-                end_loc = input('Please enter an end point (1-{})'.format(max))
-                if 1 <= int(end_loc) <= 4:
-                    end_loc = valid_end_loc[int(end_loc)]
-                    print(end_loc)
+            if valid_entry(start_loc) is False:
+                continue
+            start_loc = to_numeric(start_loc)
+            valid_end_loc = self.is_free(start_loc, ship)
+            if valid_end_loc is False:
+                continue
+            valid_end_loc, print_loc = select_dict(valid_end_loc)
+            print(print_loc)
+            max = len(valid_end_loc)
+            end_loc = input('Please enter an end point (1-{})'.format(max))
+            if 1 <= int(end_loc) <= 4:
+                end_loc = valid_end_loc[int(end_loc)]
+                self.place_ship_on_grid(start_loc, end_loc)
+                return
+
 
     def find_valid_end_loc(self, start_loc, ship):
         """
@@ -139,7 +157,7 @@ class Players:
         index_max = start_loc_x <= 9 and start_loc_y <= 9
 
         if index_min and index_max:
-            if self.gird[start_loc_y, start_loc_x] == 0:
+            if self.grid[start_loc_y, start_loc_x] == 0:
                 return True
         else:
             return False
@@ -149,7 +167,7 @@ class Players:
         Check if the Anchor point is free(first IF) AND at lest 1 end
         location is possible(2nd IF).
         """
-        if self.gird[start_loc[0], start_loc[1]] == 0:
+        if self.grid[start_loc[0], start_loc[1]] == 0:
             end_loc = self.find_valid_end_loc(start_loc, ship)
 
             if len(end_loc) >= 1:
@@ -158,3 +176,25 @@ class Players:
                 return False
         else:
             return False
+
+    def place_ship_on_grid(self, start, end):
+        """
+        Once a start location and an end location was selected, do the
+        actual placement on the players grid. Changes the state from 0 to 1
+        on the matrix in-place.
+        :param start:
+        :param end:
+        :return: Nothing
+        """
+        vertical = end[0] - start[0]
+        horizontal = end[1] - start[1]
+
+        if horizontal:
+            fixed_column = start[0]
+            for i in range(start[1], end[1]):
+                self.grid[fixed_column][i] = 1
+
+        if vertical:
+            fixed_column = start[1]
+            for i in range(start[0], end[0]):
+                self.grid[fixed_column][i] = 1
