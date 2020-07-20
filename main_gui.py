4f798bed1d5eaf91d7806b2a51d6d82f61576cb7
Peter-AK <pioter26@hotmail.com>
2020-07-19 22:53:02 -0400

    2020-July-19: Updates to GUI setup and combat

--- main_gui.py
+++ main_gui.py
@@ -1,57 +1,6 @@
 import sys
-from PyQt5 import QtWidgets as qtw
-from PyQt5 import QtGui as qtg
-from PyQt5 import QtCore as qtc
-from static_func import *
 from players import Player
-import numpy as np
-
-
-def update_interface_gird_values(obj, grid, rest):
-    for button_id in range(100, 200, 1):
-        button_str = str(button_id)
-        y = int(button_str[1])
-        x = int(button_str[2])
-        obj.player_group.button(button_id).setText(
-            str(grid[y][x]))
-        if grid[y][x] == 1:
-            obj.player_group.button(button_id).setStyleSheet("background-color: "
-                                                     "#808080")
-        elif grid[y][x] == 555 and rest == True:
-            obj.player_group.button(button_id).setStyleSheet("background-color: "
-                                                     "None")
-
-
-def clear_style_sheet(obj):
-    for i in range(100, 199, 1):
-        obj.player_group.button(i).setStyleSheet("background-color: "
-                                                  "None")
-
-
-def grid_to_gid(gird_loc):
-    gid = 100 + int(str(gird_loc[0]) + str(gird_loc[1]))
-    return gid
-
-
-def gid_to_grid(gid):
-    button_id = str(gid)
-    button_id = [int(button_id[1]), int(button_id[2])]
-    return button_id
-
-
-def add_buttons(grid, ischeckable, group):
-    positions = [[i, j] for i in range(10) for j in range(10)]
-    for position in positions:
-        gid = grid_to_gid([position[0], position[1]])
-        loc = str(to_alpha_numeric(position))
-        loc = loc.replace("['", "")
-        loc = loc.replace("']", "")
-        button = qtw.QPushButton("{}".format(loc))
-        button.setFixedHeight(40)
-        button.setFixedWidth(60)
-        button.setCheckable(ischeckable)
-        grid.addWidget(button, *position)
-        group.addButton(button, id=gid)
+from static_gui_funcs import *
 
 
 class SplashScreen(qtw.QWidget):
@@ -92,7 +41,6 @@ class SplashScreen(qtw.QWidget):
         self.quit_btn.clicked.connect(self.quit_now)
 
         # Frame settings
-        self.move(800, 300)
         self.setWindowTitle("BattleShip!")
         self.center()
         self.show()
@@ -102,6 +50,8 @@ class SplashScreen(qtw.QWidget):
         sys.exit()
 
     def start_setup(self):
+        global setup
+        setup = Setup()
         setup.show()
         self.close()
 
@@ -112,74 +62,92 @@ class SplashScreen(qtw.QWidget):
         self.move(qr.topLeft())
 
 
-class Window(qtw.QWidget):
-    def __init__(self):
+class Combat(qtw.QWidget):
+    def __init__(self, obj):
         super().__init__()
-        self.test_value = np.full((10, 10), 555)
         self.selected_locations = []
-        self.selected_sum = 0
         self.selected_limit = 7
-        main_layout = qtw.QGridLayout()
+        self.max_limit = 7
+        self.combat_layout = qtw.QGridLayout()
+        self.setLayout(self.combat_layout)
+        self.setup_obj = obj
 
-        # Player's gird
-        player_widget = qtw.QWidget()
-        player_layout = qtw.QGridLayout()
-        player_widget.setLayout(player_layout)
         # Enemy gird
-        pc_widget = qtw.QWidget()
-        pc_layout = qtw.QGridLayout()
-        pc_widget.setLayout(pc_layout)
+        self.pc_widget = qtw.QWidget()
+        self.pc_layout = qtw.QGridLayout()
+        self.pc_widget.setLayout(self.pc_layout)
 
         # pc label
-        pc_label = qtw.QLabel()
-        pc_label.setText('PC:')
-        pc_label.setAlignment(qtc.Qt.AlignCenter)
-        # Player : name label
-        player_label = qtw.QLabel()
-        player_label.setText('Player:')
-        player_label.setAlignment(qtc.Qt.AlignCenter)
+        self.pc_label = qtw.QLabel()
+        self.pc_label.setText('PC:')
+        self.pc_label.setFont(qtg.QFont('Impact', 16))
+        self.pc_label.setAlignment(qtc.Qt.AlignCenter)
 
         # Fire button
         self.fire_button = qtw.QPushButton('Fire the Missiles')
 
+        # LCD num
+        self.salvo_label = qtw.QLabel('Available salvos:')
+        self.salvo_lcd = qtw.QLCDNumber()
+
         #  Logic
-        self.player_group = qtw.QButtonGroup(self)
+        self.pc_group_widget = qtw.QWidget()
+        self.pc_group_layout = qtw.QGridLayout()
+        self.pc_group_widget.setLayout(self.pc_group_layout)
         self.pc_group = qtw.QButtonGroup(self)
-        add_buttons(player_layout, False, self.player_group)
-        add_buttons(pc_layout, True, self.pc_group)
+        add_buttons(self.pc_group_layout, True, self.pc_group)
         self.pc_group.setExclusive(False)
         self.pc_group.buttonClicked.connect(self.grid_space_selected)
         self.fire_button.clicked.connect(self.fire_button_action)
-        self.update_player_button_value()
+
         # Layout
-        self.setLayout(main_layout)
+        # Player's widget space
+        self.player_widget = qtw.QWidget()
+        self.import_layout = qtw.QGridLayout()
+        self.import_layout.addWidget(self.setup_obj.player_label, 0, 1)
+        self.import_layout.addWidget(self.setup_obj.player_widget, 3, 1)
+        self.import_layout.addWidget(self.setup_obj.console_label, 4, 1)
+        add_horizontal_coordinates_label(self.import_layout, 2, 1)
+        add_vertical_coordinates_label(self.import_layout, 3, 0)
+        self.player_widget.setLayout(self.import_layout)
 
-        main_layout.addWidget(pc_label, 0, 0)
-        main_layout.addWidget(pc_widget, 1, 0)
-        main_layout.addWidget(player_label, 2, 0)
-        main_layout.addWidget(player_widget, 3, 0)
-        main_layout.addWidget(self.fire_button, 0, 2)
+        # Pc layout
+        self.pc_layout.addWidget(self.pc_label, 0, 1)
+        add_horizontal_coordinates_label(self.pc_layout, 1, 1)
+        add_vertical_coordinates_label(self.pc_layout, 2, 0)
+        self.pc_layout.addWidget(self.pc_group_widget, 2, 1)
 
-        self.move(500, 100)
-        self.center()
+        # Controls layout
+        self.controls_widget = qtw.QWidget()
+        self.controls_layout = qtw.QVBoxLayout()
+        self.controls_widget.setLayout(self.controls_layout)
+        self.controls_layout.addWidget(self.fire_button)
+        self.controls_layout.addWidget(self.salvo_label)
+        self.controls_layout.addWidget(self.salvo_lcd)
+
+
+        # Adding all Widgets to main grid layout
+        self.combat_layout.addWidget(self.player_widget, 0, 1)
+        self.combat_layout.addWidget(self.pc_widget, 0, 2)
+        self.combat_layout.addWidget(self.controls_widget, 0, 3)
+        self.combat_layout.addWidget(self.setup_obj.console_label, 1, 1, 1, 2)
+
+        self.move(200, 200)
         self.setWindowTitle("BattleShip!")
 
     def grid_space_selected(self, gid):
         if gid.isChecked():
-            self.selected_sum += 1
-            print(self.selected_sum)
+            self.selected_limit -= 1
+            self.salvo_lcd.display(str(self.selected_limit))
             self.selected_locations.append(self.pc_group.id(gid))
         elif gid.isChecked() is False:
-            self.selected_sum -= 1
+            self.selected_limit += 1
             self.selected_locations.remove(self.pc_group.id(gid))
-            print(self.selected_sum)
+            self.salvo_lcd.display(str(self.selected_limit))
 
     def fire_button_action(self):
-        if self.selected_sum < self.selected_limit:
+        if self.selected_limit >= 0:
             print(self.selected_locations)
-            # print([i for i, button in
-            #        enumerate(self.pc_group.buttons()) if
-            #        button.isChecked()])
         else:
             print('Too many locations are selected')
 
@@ -197,14 +165,43 @@ class Window(qtw.QWidget):
                 self.pc.button(gid).setText(
                     str(self.test_value[y][x]))
 
-    def show_window(self):
-        self.show()
-
     def center(self):
         qr = self.frameGeometry()
         cp = qtw.QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    def paintEvent(self, event):
+        qp = qtg.QPainter()
+        qp.begin(self)
+        self.draw_rect(qp)
+        qp.end()
+
+    def draw_rect(self, qp):
+        for rect in self.setup_obj.ship_rect_obj:
+            pos1 = [rect[0].pos().x(), rect[0].pos().y()]
+            pos2 = [rect[1].pos().x(), rect[1].pos().y()]
+
+            qp.setPen(qtg.QPen(qtc.Qt.black, 8, qtc.Qt.SolidLine))
+            width = pos2[0] - pos1[0]
+            height = pos2[1] - pos1[1]
+
+            if height == 0:
+                height = 45
+                if width > 0:
+                    qp.drawRect(pos1[0] + 45, pos1[1] + 80, width + 65,
+                                height)
+                else:
+                    qp.drawRect(pos1[0] + 110, pos1[1] + 80, width - 65,
+                                height)
+            elif width == 0:
+                width = 60
+                if height > 0:
+                    qp.drawRect(pos1[0] + 47, pos1[1] + 80, width, height +
+                                45)
+                else:
+                    qp.drawRect(pos1[0] + 47, pos1[1] + 125, width, height -
+                                45)
 
 
 class Setup(qtw.QWidget):
@@ -214,52 +211,19 @@ class Setup(qtw.QWidget):
         self.main_layout = qtw.QGridLayout()
         self.end_loc = []
         self.anchor_point = []
-
-        # Player's gird
-        player_widget = qtw.QWidget()
-        player_layout = qtw.QGridLayout()
-        player_widget.setLayout(player_layout)
+        self.anchor_obj = None
+        self.ship_rect_obj = []
 
         # Player : name label
-        player_label = qtw.QLabel()
-        player_label.setFont(qtg.QFont('Impact', 16))
-        player_label.setText('Your grid : ')
-        player_label.setAlignment(qtc.Qt.AlignCenter)
+        self.player_label = qtw.QLabel()
+        self.player_label.setFont(qtg.QFont('Impact', 16))
+        self.player_label.setText('Your grid : ')
+        self.player_label.setAlignment(qtc.Qt.AlignCenter)
 
-        # Place holder label
-        ships_label = qtw.QLabel()
-        ships_label.setFont(qtg.QFont('monospace [Consolas]', 18))
-        ships_label.setText('Select a ship : ')
-        ships_label.setAlignment(qtc.Qt.AlignCenter)
-
-        # BattleShip button group
-        self.ship_list = qtw.QWidget()
-        self.ship_layout = qtw.QVBoxLayout()
-        self.ship_list.setLayout(self.ship_layout)
-        self.ship_setup_group = qtw.QButtonGroup(self)
-
-        self.battleship = qtw.QRadioButton()
-        self.cruiser = qtw.QRadioButton()
-        self.sub = qtw.QRadioButton()
-        self.reset = qtw.QPushButton('Reset placement')
-
-        self.ship_setup_group.addButton(self.battleship, id=1)
-        self.ship_setup_group.addButton(self.cruiser, id=2)
-        self.ship_setup_group.addButton(self.sub, id=3)
-
-
-        self.ship_layout.addWidget(self.battleship)
-        self.ship_layout.addWidget(self.cruiser)
-        self.ship_layout.addWidget(self.sub)
-        self.ship_layout.addWidget(self.reset)
-
-
-        # BattleShip button group Logic
-        self.player_group = qtw.QButtonGroup()
-        add_buttons(player_layout, False, self.player_group)
-        self.player_group.buttonClicked.connect(self.setup_button_click)
-        self.reset.clicked.connect(self.reset_radio_buttons)
-        self.reset_radio_buttons()
+        # Player's gird
+        self.player_widget = qtw.QWidget()
+        self.player_layout = qtw.QGridLayout()
+        self.player_widget.setLayout(self.player_layout)
 
         # Warning label
         self.console_label = qtw.QLabel()
@@ -267,25 +231,54 @@ class Setup(qtw.QWidget):
         self.console_label.setAlignment(qtc.Qt.AlignCenter)
         self.console_label.setFont(qtg.QFont('monospace [Consolas]', 14))
 
+        # BattleShip button
+        self.player_group = qtw.QButtonGroup()
+        add_buttons(self.player_layout, False, self.player_group)
 
+        # BattleShip button group
+        self.ships_label = qtw.QLabel()
+        self.ships_label.setFont(qtg.QFont('monospace [Consolas]', 18))
+        self.ships_label.setText('Select a ship : ')
+        self.ships_label.setAlignment(qtc.Qt.AlignCenter)
+
+        self.ship_list = qtw.QWidget()
+        self.ship_layout = qtw.QVBoxLayout()
+        self.ship_button_group = qtw.QButtonGroup()
+        add_ship_buttons(self, self.player)
+
+        # Reset button
+        self.reset = qtw.QPushButton('Reset placement')
+        self.ship_layout.addWidget(self.reset)
+        self.ship_list.setLayout(self.ship_layout)
+
+        # Done button
+        self.done = qtw.QPushButton('Done', clicked=self.setup_done)
+        self.done.setFixedHeight(40)
+        self.ship_layout.addWidget(self.done)
+
+        # Logic
+        self.reset_ship_list()
+        self.player_group.buttonClicked.connect(self.setup_button_click)
+        self.reset.clicked.connect(self.reset_ship_list)
 
         # Layout
         self.setLayout(self.main_layout)
 
-        self.main_layout.addWidget(player_label, 0, 0)
-        self.main_layout.addWidget(player_widget, 1, 0)
-        self.main_layout.addWidget(ships_label, 0, 1)
-        self.main_layout.addWidget(self.ship_list, 1, 1)
-        self.main_layout.addWidget(self.console_label, 2, 0)
+        self.main_layout.addWidget(self.player_label, 0, 1)
+        self.main_layout.addWidget(self.player_widget, 2, 1)
+        self.main_layout.addWidget(self.ships_label, 0, 2)
+        self.main_layout.addWidget(self.ship_list, 2, 2)
+        self.main_layout.addWidget(self.console_label, 3, 1)
+        add_horizontal_coordinates_label(self.main_layout, 1, 1)
+        add_vertical_coordinates_label(self.main_layout, 2, 0)
 
         update_interface_gird_values(self, self.player.grid, False)
-        self.move(200, 200)
+        self.center()
         self.setWindowTitle("BattleShip!")
 
     def setup_button_click(self, button_obj):
-        clear_style_sheet(self)
         selected_button = gid_to_grid(self.player_group.id(button_obj))
-        ship_id = self.ship_setup_group.checkedId()
+        ship_id = self.ship_button_group.checkedId()
         if ship_id != -1:
             ship_obj = self.player.get_ship_by_id(ship_id)
 
@@ -304,8 +297,9 @@ class Setup(qtw.QWidget):
 
         elif end_point_check:
             self.player.place_ship(ship_obj, self.anchor_point, selected_button)
+            self.ship_rect_obj.append([self.anchor_obj, button_obj])
             self.console_label.setStyleSheet("background-color: #00ffa5")
-            self.console_label.setText(ship_obj.name + " Has been placed")
+            self.console_label.setText(ship_obj.name + " has been placed")
             self.placed_ship_radio_btn(ship_id)
             self.anchor_point = []
             self.end_loc = []
@@ -314,20 +308,15 @@ class Setup(qtw.QWidget):
             self.end_loc, print_loc = self.get_end_loc(selected_button,
                                                        ship_obj)
             self.anchor_point = selected_button
+            self.anchor_obj = button_obj
             self.console_label.setText(str(print_loc))
             self.console_label.setStyleSheet("background-color: #00ffa5")
-            self.set_end_loc_buttons(self.end_loc)
 
         update_interface_gird_values(self, self.player.grid, False)
+        self.update()
 
     def get_end_loc(self, selected_button, ship):
         return self.player.set_anchor(ship, selected_button)
-
-    def set_end_loc_buttons(self, end_loc):
-        for i in end_loc:
-            gid = 100 + (10 * i[0]) + i[1]
-            self.player_group.button(gid).setStyleSheet("background-color: "
-                                                        "red")
 
     def is_free_grid(self, selected_button):
         if self.player.grid[selected_button[0], selected_button[1]] == 555:
@@ -343,32 +332,78 @@ class Setup(qtw.QWidget):
             return False
 
     def placed_ship_radio_btn(self, ship_id):
-        self.ship_setup_group.setExclusive(False)
-        self.ship_setup_group.button(ship_id).setChecked(False)
-        self.ship_setup_group.button(ship_id).setEnabled(False)
-        self.ship_setup_group.button(ship_id).setText('Placed')
-        self.ship_setup_group.setExclusive(True)
+        self.ship_button_group.setExclusive(False)
+        self.ship_button_group.button(ship_id).setChecked(False)
+        self.ship_button_group.button(ship_id).setEnabled(False)
+        self.ship_button_group.button(ship_id).setText('Placed')
+        self.ship_button_group.setExclusive(True)
 
-    def reset_radio_buttons(self):
-        self.ship_setup_group.setExclusive(False)
-        for i in self.ship_setup_group.buttons():
+    def reset_ship_list(self):
+        ship_list_reset_text(self, self.player.my_ships)
+        self.ship_button_group.setExclusive(False)
+        for i in self.ship_button_group.buttons():
             i.setChecked(False)
             i.setEnabled(True)
-        self.ship_setup_group.setExclusive(True)
-        self.battleship.setText('Battleship (size : 5)')
-        self.cruiser.setText('Cruiser (size : 3)')
-        self.sub.setText('Submarine (size : 1)')
+        self.ship_button_group.setExclusive(True)
         self.player = Player('Player')
         self.anchor_point = []
         self.end_loc = []
+        self.ship_rect = []
+        self.ship_rect_obj = []
+        self.update()
+        self.console_label.setText('Board was reset')
+        self.console_label.setStyleSheet("background-color: #00ffa5")
         update_interface_gird_values(self, self.player.grid, True)
 
+    def paintEvent(self, event):
+        qp = qtg.QPainter()
+        qp.begin(self)
+        self.draw_rect(qp)
+        qp.end()
+
+    def draw_rect(self, qp):
+        for rect in self.ship_rect_obj:
+            pos1 = [rect[0].pos().x(), rect[0].pos().y()]
+            pos2 = [rect[1].pos().x(), rect[1].pos().y()]
+
+            qp.setPen(qtg.QPen(qtc.Qt.black, 8, qtc.Qt.SolidLine))
+            width = pos2[0] - pos1[0]
+            height = pos2[1] - pos1[1]
+
+            if height == 0:
+                height = 45
+                if width > 0:
+                    qp.drawRect(pos1[0] + 45, pos1[1] + 80, width + 65,
+                                height)
+                else:
+                    qp.drawRect(pos1[0] + 110, pos1[1] + 80, width - 65,
+                                height)
+            elif width == 0:
+                width = 60
+                if height > 0:
+                    qp.drawRect(pos1[0] + 47, pos1[1] + 80, width, height +
+                                45)
+                else:
+                    qp.drawRect(pos1[0] + 47, pos1[1] + 125, width, height -
+                                45)
+
+    def setup_done(self):
+        global combat
+        combat = Combat(self)
+        self.hide()
+        combat.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = qtw.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
-    setup = Setup()
-    run = Window()
     splash = SplashScreen()
+    setup = None
+    combat = None
     sys.exit(app.exec())
 
---
+++ static_gui_funcs.py
@@ -0,0 +1,168 @@
+from static_func import to_alpha_numeric
+
+
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtGui as qtg
+from PyQt5 import QtCore as qtc
+
+
+def update_interface_gird_values(obj, grid, rest):
+    for button_id in range(100, 200, 1):
+        button_str = str(button_id)
+        y = int(button_str[1])
+        x = int(button_str[2])
+
+        obj.player_group.button(button_id).setStyleSheet(
+            "background-color: #72bcd4")
+        obj.player_group.button(button_id).setText(
+            str(grid[y][x]))
+
+        if grid[y][x] == 1:
+            obj.player_group.button(button_id).setStyleSheet(
+                "background-color: #808080")
+        elif grid[y][x] == 555 and rest is True:
+            obj.player_group.button(button_id).setStyleSheet(
+                "background-color: None")
+        anchor_update(obj, button_id)
+        end_loc_update(obj, button_id)
+
+
+def end_loc_update(obj, button_id):
+    """
+    Updates the possible end locations in red on the players setup grid gui.
+    :param obj: gui window object that has a button group
+    :param button_id: the button id of the button group (int : 110)
+    :return:
+    """
+    for loc in obj.end_loc:
+        if loc == gid_to_grid(button_id):
+            obj.player_group.button(button_id).setStyleSheet(
+                "background-color: red")
+
+
+def anchor_update(obj, button_id):
+    """
+    Updates the anchor point on the players setup grid gui.
+    :param obj: gui window object that has a button group
+    :param button_id: the button id of the button group (int : 110)
+    :return:
+    """
+    loc = obj.anchor_point
+    if loc == gid_to_grid(button_id):
+        obj.player_group.button(button_id).setStyleSheet(
+            "background-color: #ffa500")
+
+
+def grid_to_gid(gird_loc):
+    """ grid  """
+    gid = 100 + int(str(gird_loc[0]) + str(gird_loc[1]))
+    return gid
+
+
+def gid_to_grid(gid):
+    button_id = str(gid)
+    button_id = [int(button_id[1]), int(button_id[2])]
+    return button_id
+
+
+def add_horizontal_coordinates_label(layout, row, col):
+    """
+    adds a horizontal/vertical widget with all the labels for the girds to
+    a layout
+    :param layout:
+    :param row: row in QGridLayout
+    :param col: col in QGridLayout
+    :return:
+    """
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    horizontal_widget = qtw.QWidget()
+    horizontal_layout = qtw.QHBoxLayout()
+    horizontal_widget.setLayout(horizontal_layout)
+    for i in letters:
+        alpha_label = qtw.QLabel(i)
+        alpha_label.setAlignment(qtc.Qt.AlignCenter)
+        horizontal_layout.addWidget(alpha_label)
+    layout.addWidget(horizontal_widget, row, col)
+
+
+def add_vertical_coordinates_label(layout, row, col):
+    """
+    adds a horizontal/vertical widget with all the labels for the girds to
+    a layout
+    :param layout:
+    :param row: row in QGridLayout
+    :param col: col in QGridLayout
+    :return:
+    """
+    vertical_widget = qtw.QWidget()
+    vertical_layout = qtw.QVBoxLayout()
+    vertical_widget.setLayout(vertical_layout)
+
+    for i in range(1, 11, 1):
+        num_label = qtw.QLabel(str(i))
+        num_label.setAlignment(qtc.Qt.AlignCenter)
+        vertical_layout.addWidget(num_label)
+    layout.addWidget(vertical_widget, row, col)
+
+
+def add_buttons(layout, ischeckable, group):
+    """
+    Creates buttons and adds them to a QButtonGroup then sets them in a
+    QGridLayout.
+    :param layout: QGridLayout
+    :param ischeckable: Bool
+    :param group: QButtonGroup
+    :return:
+    """
+    positions = [[i, j] for i in range(10) for j in range(10)]
+    for position in positions:
+        gid = grid_to_gid([position[0], position[1]])
+        loc = str(to_alpha_numeric(position))
+        loc = loc.replace("['", "")
+        loc = loc.replace("']", "")
+        button = qtw.QPushButton("{}".format(loc))
+        button.setFixedHeight(40)
+        button.setFixedWidth(60)
+        button.setCheckable(ischeckable)
+        layout.addWidget(button, *position)
+        group.addButton(button, id=gid)
+
+
+def add_ship_buttons(obj, player):
+    """
+    Creates QRadioButtons and adds them to a QButtonGroup then sets them in a
+    respective layout.
+    :param obj: Object that has the button group and layout
+    :param player: player object
+    :return:
+    """
+
+    for ship in player.my_ships:
+        name = ship.name
+        size = ship.size
+        button = qtw.QRadioButton(f"{name} : Size : {size}")
+        obj.ship_layout.addWidget(button)
+        obj.ship_button_group.addButton(button, id=ship.id)
+
+
+def ship_list_reset_text(obj, ships):
+    """
+    Resets the ship radio button text when the reset button is pressed.
+    :param obj:
+    :param ships:
+    :return:
+    """
+    for ship in ships:
+        name = ship.name
+        size = ship.size
+        obj.ship_button_group.button(ship.id).setText(
+            f"{name} : \n Size : {size}")
+
+
+def add_rect(obj, anchor_point_obj, end_point_obj):
+    pos1 = [anchor_point_obj.pos().x(), anchor_point_obj.pos().y()]
+
+    pos2 = [end_point_obj.pos().x(), end_point_obj.pos().y()]
+
+    obj.ship_rect.append([pos1, pos2])
+    obj.update()
